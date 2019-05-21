8fd7ca711b6895e191fc1d0f931b5f4e4e0a8fe2
Azais Corentin <azaiscorentin@gmail.com>
2019-05-21 17:06:23 +0200

    Remove the scheduler sleep feature

--- src/fengine/scheduler.cpp
+++ src/fengine/scheduler.cpp
@@ -33,20 +33,6 @@ void Scheduler::update()
             m_tasks[i](ticker.getDelta());
         }
     }
-
-    // Sleep until the next update is needed
-    timing::Clock::duration timeToWait = timing::Clock::duration::max();
-    const auto timeNow                 = timing::Clock::now();
-
-    for (size_t i = 0; i < m_tasks.size(); i++) {
-        auto&& ticker = m_tickers.at(i);
-
-        // Find the closest update
-        timeToWait = std::min(timeToWait, ticker.next());
-    }
-
-    if (timeToWait > std::chrono::milliseconds(3))
-        std::this_thread::sleep_for(timeToWait - std::chrono::milliseconds(3));
 }
 
 } // namespace frames
